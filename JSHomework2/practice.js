7ff8d2d30cfbbc5130aa46f6fd4428ce219b832f
Rishov Chatterjee <garpchoo@gmail.com>
2017-04-22 02:52:48 -0700

    Second Homework Added
    This JS Homework assignment focuses on mastery of name-value pairs, arrays, and node modules.

---
+++ JSHomework2/practice.js
@@ -0,0 +1,12 @@
+/**
+
+Task 1: Export profile.js to this file.
+
+Task 2: Create a function that will output "Jason Bourne is awesome"
+in the terminal by using elements in profile.js
+
+Task 3: Create a function that will output in a new file called
+statement.txt :
+"Jason Bourne was a Black Ops Assassin in Treadstone, Iron Hand, Black Water."
+
+**/
---
+++ JSHomework2/profile.js
@@ -0,0 +1,32 @@
+var bourne = {
+  name: 'Jason Bourne',
+  actor: 'Matt Damon',
+  movies: [
+    'Bourne Legacy',
+    'Bourne Supremacy',
+    'Bourne Ultimatum',
+    'Bourne Identity'
+  ],
+  occupation: {
+    job1: 'CIA Operative',
+    job2: 'Black Ops Assassin',
+    job3: 'Investigator',
+    job4: null
+  },
+  keywords: [
+    'awesome',
+    'magnificent',
+    'outstanding',
+    'terrible',
+    'horribly bad',
+    'freaking awesome'
+  ],
+  programs: [
+    'Treadstone',
+    'Iron Hand',
+    'Dark Water'
+  ]
+
+}
+
+module.exports.bio = bourne;
