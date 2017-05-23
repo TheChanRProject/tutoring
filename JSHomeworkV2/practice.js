3bd4bf1323bc12cad3f0b99609ab6b47e30fa667
Rishov Chatterjee <garpchoo@gmail.com>
2017-05-23 11:57:20 -0700

    Added JSHomeworkV2 for beginners
    This homework teaches you more about arrays and creating functions.

---
+++ JSHomeworkV2/practice.js
@@ -0,0 +1,50 @@
+/**
+
+Resources:
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+
+Task 1:
+
+Create two functions:
+
+Function 1: Divide the number by 5 ( / )
+
+Function 2: Take the square root of a number.
+
+Hint: Use Math.sqrt() in the function
+
+Task 2:
+
+Change the current array so that each number has been
+divided by 5.
+
+Add the numbers in the new array
+
+Take the square root of the number
+
+**/
+
+function divide5() {
+
+}
+divide5(5)
+
+function sqrt() {
+
+}
+sqrt(4)
+
+
+var array = [0,1,2,3,4,5,6,7,9,10]
+
+
+var newArray = array.
+
+console.log(newArray);
+
+var sum = newArray.
+
+
+console.log();
